80c27a92a6b68287c6edf1b649a98744bd8b783d
Ranjit47H <ranjit.u2h@gmail.com>
2024-06-30 08:54:31 +0530

    making dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,10 +16,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-        <Route path="/projects" element={<Projects />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-
 export default function ThemeProvider({ children }) {
   const { theme } = useSelector((state) => state.theme);
   return (
